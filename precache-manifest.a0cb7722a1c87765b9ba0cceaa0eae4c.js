c65bb888c08093bff3331944866a456c45d8ba50
bijoyalfred1983 <bijoyalfred1983@gmail.com>
2018-10-04 20:59:19 +0530

    Updates

---
+++ precache-manifest.a0cb7722a1c87765b9ba0cceaa0eae4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f2988afc7cd3ef40830",
+    "url": "/first-app/static/js/runtime~main.7f2988af.js"
+  },
+  {
+    "revision": "d65aa54e5da966f50607",
+    "url": "/first-app/static/js/main.d65aa54e.chunk.js"
+  },
+  {
+    "revision": "128eedada1cf9857f36f",
+    "url": "/first-app/static/js/1.128eedad.chunk.js"
+  },
+  {
+    "revision": "d65aa54e5da966f50607",
+    "url": "/first-app/static/css/main.717fcba9.chunk.css"
+  },
+  {
+    "revision": "36a8b0cc9314be85b95d6fe52212d656",
+    "url": "/first-app/index.html"
+  }
+];

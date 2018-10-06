c97bf52d363da487f50ba67fbd44e05f03ac5645
bijoyalfredrocks <bijoyalfred1983@gmail.com>
2018-10-06 12:30:28 +0530

    changes in render method

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      displayLoader: true
+    }
+  }
   componentDidMount(){
+    let loader = document.getElementById('intialLoader');
+    loader.style.display = "none";
+    this.setState({displayLoader:false})
 
-    setTimeout(()=>{
-      let loader = document.getElementById('intialLoader');
-      loader.style.display = "none";
-    },2000)
-
+  }
+  displayPage(){
+    return(
+      <div className="App">
+      <div className="App-header"> 
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+    </div>
+    )
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+     <div>{
+       !this.state.displayLoader ? this.displayPage() :null
+           
+     }    }</div>
+
     );
   }
 }
